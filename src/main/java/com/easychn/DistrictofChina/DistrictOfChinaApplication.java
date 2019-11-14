6d44667ecd12aa75ea63624670fc9e4aed579bbb
Jason <jasonwell@139.com>
2019-11-14 16:21:21 +0800

    初始化spring boot项目

---
+++ src/main/java/com/easychn/DistrictofChina/DistrictOfChinaApplication.java
@@ -0,0 +1,13 @@
+package com.easychn.DistrictofChina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistrictOfChinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistrictOfChinaApplication.class, args);
+	}
+
+}
